ae14cc1fb5a462cc993379fa33dbaa2a5ae2c048
Milan Momcilovic <m.momcilovic@beeit.rs>
2026-09-09 07:22:24 +0200

    Restore Shopify auth flow

--- app/routes/auth.login/route.jsx
+++ app/routes/auth.login/route.jsx
@@ -1,29 +1,48 @@
-export const loader = async () => {
-  return null;
+import { AppProvider } from "@shopify/shopify-app-react-router/react";
+import { useState } from "react";
+import { Form, useActionData, useLoaderData } from "react-router";
+import { login } from "../../shopify.server";
+import { loginErrorMessage } from "./error.server";
+
+export const loader = async ({ request }) => {
+  const errors = loginErrorMessage(await login(request));
+
+  return { errors };
 };
 
-export const action = async () => {
-  return new Response("LOGIN ACTION WORKS", {
-    status: 200,
-    headers: {
-      "Content-Type": "text/plain",
-    },
-  });
+export const action = async ({ request }) => {
+  const errors = loginErrorMessage(await login(request));
+
+  return {
+    errors,
+  };
 };
 
 export default function Auth() {
+  const loaderData = useLoaderData();
+  const actionData = useActionData();
+  const [shop, setShop] = useState("");
+  const { errors } = actionData || loaderData;
+
   return (
-    <div style={{ padding: 40 }}>
-      <h1>AUTH TEST VERSION 12345</h1>
+    <AppProvider embedded={false}>
+      <s-page>
+        <Form method="post">
+          <s-section heading="Log in">
+            <s-text-field
+              name="shop"
+              label="Shop domain"
+              details="example.myshopify.com"
+              value={shop}
+              onChange={(e) => setShop(e.currentTarget.value)}
+              autocomplete="on"
+              error={errors.shop}
+            ></s-text-field>
 
-      <form method="post">
-        <input
-          name="shop"
-          defaultValue="practicestore-jv2xvjm6.myshopify.com"
-        />
-
-        <button type="submit">Test POST</button>
-      </form>
-    </div>
+            <s-button type="submit">Log in</s-button>
+          </s-section>
+        </Form>
+      </s-page>
+    </AppProvider>
   );
 }
